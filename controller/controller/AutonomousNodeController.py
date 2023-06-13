741158562ebf56f7c36987c683f934f4f9c46a35
joeldushouyu <joeldushouyu@gmail.com>
2023-06-13 16:25:38 -0400

    feat: upate logic for gps waypoint follower

--- controller/controller/AutonomousNodeController.py
+++ controller/controller/AutonomousNodeController.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 import time
+from geographiclib.geodesic import Geodesic
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import NavSatFix
 from nav_msgs.msg import Odometry
@@ -22,18 +23,55 @@ from std_msgs.msg import Bool
 import rclpy
 from rclpy.duration import Duration
 from rclpy.node import Node
+import math 
 
 """
 Basic navigation demo to go to pose.
 """
 
 
-import geopy.distance
 
-coords_1 = (34.841386, -82.411834)
-coords_2 = (34.841305, -82.411692)
+def calc_goal( origin_lat, origin_long, goal_lat, goal_long):
+    # Calculate distance and azimuth between GPS points
+    geod = Geodesic.WGS84  # define the WGS84 ellipsoid
+    g = geod.Inverse(origin_lat, origin_long, goal_lat, goal_long) # Compute several geodesic calculations between two GPS points 
+    hypotenuse = distance = g['s12'] # access distance
+    
+    
+    
+    degree_to_rad = float(math.pi / 180.0)
+
+    d_lat = (goal_lat - origin_lat) * degree_to_rad
+    d_long = (goal_long - origin_long) * degree_to_rad
+
+    a = pow(sin(d_lat / 2), 2) + cos(origin_lat * degree_to_rad) * cos(goal_lat * degree_to_rad) * pow(sin(d_long / 2), 2)
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    km = 6367 * c
+    mi = 3956 * c
+
+    #self.get_logger().info("The distance from the origin to the goal is {:.3f} m.".format(distance))
+    azimuth = 90- g['azi1'] #https://answers.ros.org/question/219182/how-to-determine-yaw-angle-from-gps-bearing/
+    #self.get_logger().info("The azimuth from the origin to the goal is {:.3f} degrees.".format(azimuth))
+
+    # Convert polar (distance and azimuth) to x,y translation in meters (needed for ROS) by finding side lenghs of a right-angle triangle
+    # Convert azimuth to radians
+    print(azimuth)
+    azimuth = math.radians(azimuth)
+    x = adjacent = math.cos(azimuth) * hypotenuse
+    y = opposite = math.sin(azimuth) * hypotenuse
+    #self.get_logger().info("The translation from the origin to the goal is (x,y) {:.3f}, {:.3f} m.".format(x, y))
+
+    return x, y
+
+import geopy.distance
+from math import sin, cos, atan, sqrt, atan2
+coords_1 = (34.841398, -82.411860)
+coords_2 = (34.841323, -82.411724)
 
 coord_list = [coords_1, coords_2]
+
+
+
 class AutonomousNodeController(Node):
 
     def __init__(self):
@@ -56,6 +94,39 @@ class AutonomousNodeController(Node):
             Odometry, "odometry/global", self.update_latest_map_odom, 10
         )
 
+    
+    
+    def calc_goal(self, origin_lat, origin_long, goal_lat, goal_long):
+        # Calculate distance and azimuth between GPS points
+        geod = Geodesic.WGS84  # define the WGS84 ellipsoid
+        g = geod.Inverse(origin_lat, origin_long, goal_lat, goal_long) # Compute several geodesic calculations between two GPS points 
+        hypotenuse = distance = g['s12'] # access distance
+        
+        
+        
+        degree_to_rad = float(math.pi / 180.0)
+
+        d_lat = (goal_lat - origin_lat) * degree_to_rad
+        d_long = (goal_long - origin_long) * degree_to_rad
+
+        a = pow(sin(d_lat / 2), 2) + cos(origin_lat * degree_to_rad) * cos(goal_lat * degree_to_rad) * pow(sin(d_long / 2), 2)
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        km = 6367 * c
+        mi = 3956 * c
+
+        self.get_logger().info("The distance from the origin to the goal is {:.3f} m.".format(distance))
+        azimuth = 90-g['azi1'] # yaw offset of ned to enu 
+                            # https://answers.ros.org/question/219182/how-to-determine-yaw-angle-from-gps-bearing/
+        self.get_logger().info("The azimuth from the origin to the goal is {:.3f} degrees.".format(azimuth))
+
+        # Convert polar (distance and azimuth) to x,y translation in meters (needed for ROS) by finding side lenghs of a right-angle triangle
+        # Convert azimuth to radians
+        azimuth = math.radians(azimuth)
+        x = adjacent = math.cos(azimuth) * hypotenuse
+        y = opposite = math.sin(azimuth) * hypotenuse
+        self.get_logger().info("The translation from the origin to the goal is (x,y) {:.3f}, {:.3f} m.".format(x, y))
+
+        return x, y
     def update_latest_map_odom(self, message: Odometry):
         self.current_map_frame_odom = message
 
@@ -120,8 +191,11 @@ class AutonomousNodeController(Node):
             goal_pose = PoseStamped()
             goal_pose.header.frame_id = 'map'
             goal_pose.header.stamp = self.navigator.get_clock().now().to_msg()
-            goal_pose.pose.position.x = geopy.distance.geodesic(self.latest_gps_coor, point).m
-            goal_pose.pose.position.y = 0.0
+            #geopy.distance.geodesic(self.latest_gps_coor, point).
+            
+            x,y = self.calc_goal(self.latest_gps_coor.latitude, self.latest_gps_coor.longitude,point[0], point[1])
+            goal_pose.pose.position.x =x
+            goal_pose.pose.position.y = y
             goal_pose.pose.orientation.w = 1.0
 
             # sanity check a valid path exists
@@ -130,7 +204,7 @@ class AutonomousNodeController(Node):
             self.navigator.goToPose(goal_pose)
             self.navigator_result()
 
-    def MoveStraight(self, distance=3.0):
+    def MoveStraight(self, distance=5.0):
         "This should be use as a test function"
         self.navigator = BasicNavigator()
         # Set our demo's initial pose
@@ -187,19 +261,19 @@ class AutonomousNodeController(Node):
             # Do something with the feedback
             i = i + 1
             feedback = self.navigator.getFeedback()
-            # if feedback and i % 5 == 0:
-            #     print('Estimated time of arrival: ' + '{0:.0f}'.format(
-            #         Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9)
-            #         + ' seconds.')
+            if feedback and i % 5 == 0:
+                print('Estimated time of arrival: ' + '{0:.0f}'.format(
+                    Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9)
+                    + ' seconds.')
 
-            #     # Some navigation timeout to demo cancellation
-            #     if Duration.from_msg(feedback.navigation_time) > Duration(seconds=600.0):
-            #         self.navigator.cancelTask()
+                # Some navigation timeout to demo cancellation
+                if Duration.from_msg(feedback.navigation_time) > Duration(seconds=600.0):
+                    self.navigator.cancelTask()
 
-            #     # Some navigation request change to demo preemption
-            #     if Duration.from_msg(feedback.navigation_time) > Duration(seconds=18.0):
-            #         goal_pose.pose.position.x = -3.0
-            #         navigator.goToPose(goal_pose)
+                # # Some navigation request change to demo preemption
+                # if Duration.from_msg(feedback.navigation_time) > Duration(seconds=18.0):
+                #     goal_pose.pose.position.x = -3.0
+                #     navigator.goToPose(goal_pose)
 
         # Do something depending on the return code
         result = self.navigator.getResult()
@@ -229,4 +303,6 @@ def main():
 
 
 if __name__ == '__main__':
+    x,y = calc_goal(coords_1[0], coords_1[1], coords_2[0], coords_2[1])
+    print(x, y)
     main()
