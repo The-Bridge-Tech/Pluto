b6efb72d7915a8fb1d6aa373190418106a92b3e3
joeldushouyu <joeldushouyu@gmail.com>
2023-06-13 16:17:30 -0400

    modify uncertanity in imu message during launch

--- pluto_launch/launch/imu.launch.py
+++ pluto_launch/launch/imu.launch.py
@@ -8,34 +8,68 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
+# use_sim_time = LaunchConfiguration('use_sim_time', default='true')
 
 
-use_sim_time = LaunchConfiguration('use_sim_time', default='true')  
+# def generate_launch_description():
+
+#     # spatial_launch = IncludeLaunchDescription(
+#     #   PythonLaunchDescriptionSource([os.path.join(
+#     #      get_package_share_directory('phidgets_spatial') ),
+#     #      '/launch/spatial-launch.py']),
+#     #     launch_arguments={
+#     #         "use_sim_time": use_sim_time
+#     #     }.items(),
+#     #   )
+
+#     spatial_launch = IncludeLaunchDescription(
+#         PythonLaunchDescriptionSource([os.path.join(
+#             get_package_share_directory('pluto_launch')),
+#             '/spatial-launch.py']),
+#         launch_arguments={
+#             'angular_velocity_stdev': '0.349056',
+#             'linear_acceleration_stdev': '0.20',
+#             'magnetic_field_stdev': '0.001658',
+#         }.items()
+
+#     )
+#     # imu_tool = IncludeLaunchDescription(
+#     #   PythonLaunchDescriptionSource([os.path.join(
+#     #      get_package_share_directory('pluto_launch')),
+#     #      '/imu_filter.launch.py'])
+#     #   )
+
+#     return LaunchDescription([
+#         spatial_launch,
+#         # imu_tool
+#     ])
+
+import launch
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
 
 def generate_launch_description():
+    """Generate launch description with multiple components."""
+    container = ComposableNodeContainer(
+        name='phidget_container',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[
+                ComposableNode(
+                    package='phidgets_spatial',
+                    plugin='phidgets::SpatialRosI',
+                    name='phidgets_spatial',
+                    parameters=[{
+                        'angular_velocity_stdev': 0.20,
+                        'linear_acceleration_stdev': 0.57,
+                        'magnetic_field_stdev': 0.001658,
+                    }]),
 
-    # spatial_launch = IncludeLaunchDescription(
-    #   PythonLaunchDescriptionSource([os.path.join(
-    #      get_package_share_directory('phidgets_spatial') ),
-    #      '/launch/spatial-launch.py']),
-    #     launch_arguments={
-    #         "use_sim_time": use_sim_time
-    #     }.items(),
-    #   )
-    
-    spatial_launch = IncludeLaunchDescription(
-      PythonLaunchDescriptionSource([os.path.join(
-         get_package_share_directory('pluto_launch') ),
-         '/spatial-launch.py']),
+        ],
 
-      )
-    # imu_tool = IncludeLaunchDescription(
-    #   PythonLaunchDescriptionSource([os.path.join(
-    #      get_package_share_directory('pluto_launch')),
-    #      '/imu_filter.launch.py'])
-    #   )
-    
-    return LaunchDescription([
-      spatial_launch,
-      # imu_tool
-   ])
+        output='both',
+    )
+
+    return launch.LaunchDescription([container])
