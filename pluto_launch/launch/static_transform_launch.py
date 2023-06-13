13ccb30711ca3f854ca018047b90dba4ccafdddb
joeldushouyu <joeldushouyu@gmail.com>
2023-06-13 16:24:21 -0400

    form temporary static tree, need to be modify later once all component at set in final position

--- pluto_launch/launch/static_transform_launch.py
+++ pluto_launch/launch/static_transform_launch.py
@@ -2,21 +2,23 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 
+#TODO: comeback to correct later
 # use_sim_time = LaunchConfiguration('use_sim_time', default='true') 
 def generate_launch_description():
     imu_to_base_link = Node(
             package="tf2_ros", executable="static_transform_publisher",
-            arguments=['-0.5588','0','0','0','0','0','base_link', 'imu_link'],
+            arguments=['0','0','0','0','0','0','base_link', 'imu_link'],
             # parameters=[{"use_sim_time":use_sim_time}]
         )
     gps_to_base_link =Node(
             package="tf2_ros", executable="static_transform_publisher",
-            arguments=['-0.5588','-0.0508','0','0','0','0','base_link', 'gps_link'],
+            #arguments=['0','0','1','0', '0','0','base_link', 'gps_link'],
+            arguments=['0','0','1','1.5707963', '0','3.1415926','base_link', 'gps_link'],  # transform NED to ENU frame
             # parameters=[{"use_sim_time":use_sim_time}]
     )
     camera_to_baselink = Node(
             package="tf2_ros", executable="static_transform_publisher",
-            arguments=['-0/75','0','0','0','0','0','base_link', 'camera_link'],
+            arguments=['0','0','0','0','0','0','base_link', 'camera_link'],
             # parameters=[{"use_sim_time":use_sim_time}]
     )
     return LaunchDescription(
