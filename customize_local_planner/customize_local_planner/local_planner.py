e6cc9f08e59c3dd58ec1bf30e16479fb3384dc8f
joeldushouyu <joeldushouyu@gmail.com>
2023-12-21 14:50:55 -0500

    decrease straight tolerance angle

--- customize_local_planner/customize_local_planner/local_planner.py
+++ customize_local_planner/customize_local_planner/local_planner.py
@@ -33,7 +33,7 @@ class LocalPlanner(Node):
         self.declare_parameter("moving_straight_ki", 0.0)
         self.declare_parameter("moving_straight_initial_pwm", 1420)
         # self.declare_parameter("moving_straight_distance_tolerance", 0.3)
-        self.declare_parameter("moving_straight_angle_threshold", 65.0)
+        self.declare_parameter("moving_straight_angle_threshold", 35.0)
         self.declare_parameter("moving_straight_forward_prediction_step", 1)
 
         self.declare_parameter("turning_kp", 2.0)
