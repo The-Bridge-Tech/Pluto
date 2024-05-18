c2bd02bbf081939014836c90cb244c9eeadb0f35
joeldushouyu <joeldushouyu@gmail.com>
2024-05-18 11:37:53 -0400

    tuning turning

--- customize_local_planner/customize_local_planner/local_planner.py
+++ customize_local_planner/customize_local_planner/local_planner.py
@@ -36,8 +36,8 @@ class LocalPlanner(Node):
         self.declare_parameter("moving_straight_angle_threshold", 35.0)
         self.declare_parameter("moving_straight_forward_prediction_step", 1)
 
-        self.declare_parameter("turning_kp", 2.0)
-        self.declare_parameter("turning_kd", 0.0)
+        self.declare_parameter("turning_kp", 3.0)
+        self.declare_parameter("turning_kd", 0.5)
         self.declare_parameter("turning_ki", 0.0)
         self.declare_parameter("turning_prediction_step", 1)
 
