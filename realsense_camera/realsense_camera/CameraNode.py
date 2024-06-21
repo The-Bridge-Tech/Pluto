daaceac8a7fcfaa76bb45cbbe0a0ddc00f8ea2be
developer <developer@thebridgetech.org>
2024-06-21 18:05:28 +0000

    Separate image classification implementation

--- realsense_camera/realsense_camera/CameraNode.py
+++ realsense_camera/realsense_camera/CameraNode.py
@@ -37,48 +37,55 @@ class CameraNode(Node):
             self.image_callback,
             10
         )
-        # Load AI image classifier model
-        # self.model = ImageClassifier.load("model1") # TODO make the model name a parameter
-        # NOTE process images synchronously such that only the most recent image sent by the camera is classified each time,
-        # this way, asynchronous requests don't pile up and processing remains in realtime
         self.processing = False
+        # Load AI image classifier model
+        # self.model = ImageClassifier.load("model1")
     
     def image_callback(self, msg: Image):
+        """Process image messages synchronously such that the most recent message is processed each time this method finishes.
+        This way, asynchronous requests won't pile up and processing remains closer to realtime."""
         # ignore the current image if there is already one being processed
         if not self.processing:
             self.processing = True
             try:
                 # Get image array from message
-                image = self.preprocess_image(msg)
+                image = self.msg_to_image_array(msg)
                 # Detect line from image
                 line = LineDetector.detectLine(image)
                 # Log the line
                 self.get_logger().info(str(line))
                 # Divide image into left and right regions based on the line
                 left_region, right_region = line.divideImage(image)
-                # Classify each region as cut or uncut
-                # left_cut = self.model.classify(left_region)
-                # right_cut = self.model.classify(right_region)
-                # Log each classification
-                # self.get_logger().info("Left side is " + "cut" if left_cut else "UNCUT")
-                # self.get_logger().info("Right side is " + "cut" if right_cut else "UNCUT")
+                # Classify the regions as cut or uncut
+                # left_cut, right_cut = self.classify_regions(left_region, right_region)
             except Exception as e:
                 self.get_logger().error(f"Error processing image: {e}")
             self.processing = False
 
-    def preprocess_image(self, msg: Image) -> np.ndarray:
-        """Convert ROS Image msg to numpy array."""
+    def msg_to_image_array(self, msg: Image) -> np.ndarray:
+        """Convert ROS Image msg to a numpy array containing the image data."""
         # Convert the Image msg to a cv2 image
         cv_image = CvBridge().imgmsg_to_cv2(msg, desired_encoding='bgr8')
         # Resize the image to 224x224 pixels
         resized_image = cv2.resize(cv_image, (224, 224))
         # Convert the image to a numpy array
-        img_array = np.array(resized_image, dtype=np.float32)
+        image_array = np.array(resized_image, dtype=np.float32)
         # Normalize the image
-        img_array /= 255.0
+        image_array /= 255.0
         # Add batch dimension
-        img_array = np.expand_dims(img_array, axis=0)
-        return img_array
+        image_array = np.expand_dims(image_array, axis=0)
+        return image_array
+    
+    def classify_regions(self, left: np.ndarray, right: np.ndarray) -> tuple[bool, bool]:
+        """Classify left and right regions as cut (True) or uncut (False).
+        Return (<left side cut?>, <right side cut?>)"""
+        # left_cut = self.model.classify(left)
+        # right_cut = self.model.classify(right)
+        # self.get_logger().info("Left side is " + "cut" if left_cut else "UNCUT")
+        # self.get_logger().info("Right side is " + "cut" if right_cut else "UNCUT")
+        # return left_cut, right_cut
+        pass
+
 
 
 # MAIN
