bea2d2f135678d9011ce8d2eddfd9640a8fbac41
Matthew <mlauriault03@gmail.com>
2024-11-01 20:44:48 +0000

    AR-1192: Create ActionClient in phase_one_demo

--- customize_local_planner/customize_local_planner/phase_one_demo.py
+++ customize_local_planner/customize_local_planner/phase_one_demo.py
@@ -1,34 +1,24 @@
 """
 Publishes path with goal poses to local_planner node
-Authors: Joel Du Shouyu & Matthew Lauriault
+Author: Matthew Lauriault
 """
 
 
 # ROS MODULES
 import rclpy
 from rclpy.node import Node
+from rclpy.action import ActionClient
+from rclpy.action.client import Future, ClientGoalHandle
 from std_msgs.msg import Bool
-from sensor_msgs.msg import NavSatFix
 from geometry_msgs.msg import Pose, PoseStamped, Point
-from nav_msgs.msg import Odometry, Path
+from nav2_msgs.action import NavigateThroughPoses
 from custom_msgs.msg import WaypointMsg
-
-# CALCULATION MODULES
-import math
-import time
 
 # HELPER MODULES
 from .conversions import *
 
 # CONSTANTS
 BASE_GPS = (34.841400, -82.411743)
-OLD_WAYPOINTS = [
-    (34.841433, -82.411767),    # front-right corner
-    (34.841367, -82.411833),    # back-right corner
-    (34.841283, -82.411717),    # back-left corner
-    (34.841350, -82.411700),    # front-left corner
-    (34.841433, -82.411767),    # front-right corner (return to #1)
-]
 WAYPOINTS = [
     (34.841384, -82.411669),    # front-left corner
     (34.841254, -82.411731),    # back-left corner
@@ -44,17 +34,14 @@ class PhaseOneDemo(Node):
     def __init__(self):
         super().__init__('PhaseOneDemo')
 
+        # ACTION CLIENT
+        self.local_plan_action_client = ActionClient(
+            self,
+            NavigateThroughPoses,
+            "/local_plan",
+        )
+
         # PUBLISHERS
-        self.local_plan_publisher = self.create_publisher(
-            Path, 
-            "local_plan",
-            10
-        )
-        self.is_pure_pursuit_controller_mode_publisher = self.create_publisher(
-            Bool, 
-            "is_pure_pursuit_controller_mode", 
-            10
-        )
         self.ping_publisher = self.create_publisher(
             WaypointMsg,
             "/waypoint_ping",
@@ -62,21 +49,6 @@ class PhaseOneDemo(Node):
         )
 
         # SUBSCRIBERS
-        self.odom_sub = self.create_subscription(
-            Odometry, 
-            "odometry/global", 
-            self.odom_callback, 
-            10
-        )
-        self.current_odom: Odometry = None
-        self.gps_sub = self.create_subscription(
-            NavSatFix, 
-            "/fix/filtered", 
-            self.gps_callback, 
-            10
-        )
-        self.initial_gps: NavSatFix = None
-        self.current_gps: NavSatFix = None
         self.is_autonomous_mode_sub = self.create_subscription(
             Bool, 
             "is_autonomous_mode",
@@ -86,20 +58,82 @@ class PhaseOneDemo(Node):
         self.is_autonomous_mode = False
 
         # TIMERS
-        timer_period = 0.3  # seconds
-        self.timer = self.create_timer(
-            timer_period, 
-            self.publish_local_plan
-        )
         self.ready_to_ping = False
         ping_timer_period = 1 # seconds
-        self.ping_timer = self.create_timer(
-            ping_timer_period,
-            self.publish_waypoint_ping
-        )
+        # self.ping_timer = self.create_timer(
+        #     ping_timer_period,
+        #     self.publish_waypoint_ping
+        # )
 
-        # Initialize goal poses and current goal pose
+        # Initialize goal poses
         self.reset()
+
+    
+    # ACTION CLIENT
+
+    def send_local_plan_goal(self, goal_msg: NavigateThroughPoses.Goal):
+        """Send action 'goal service' message (with path to navigate) to local_planner's action server."""
+        # wait for local_planner's action server
+        self.local_plan_action_client.wait_for_server()
+        # send goal asynchronously (will send when action server is ready to receive it)
+        self.goal_future = self.local_plan_action_client.send_goal_async(
+            goal_msg,
+            feedback_callback = self.local_plan_feedback_callback
+        )
+        # set callback for when response is returned
+        self.goal_future.add_done_callback(self.local_plan_goal_response_callback)
+
+
+    def local_plan_goal_response_callback(self, future: Future):
+        """Receive response from local_planner's action server after sending it a goal."""
+        # See how the goal was received by local_planner
+        goal_handle: ClientGoalHandle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().warn("Goal rejected by local_planner")
+            return
+        self.get_logger().info("Goal accepted by local_planner")
+        # request the goal result asynchronously (will receive when action server is ready to send it)
+        self.result_future: Future = goal_handle.get_result_async()
+        # set callback for when result is returned (after goal has either been completed or failed)
+        self.result_future.add_done_callback(self.local_plan_result_callback)
+        
+
+    def local_plan_feedback_callback(self, feedback_msg: NavigateThroughPoses.Impl.FeedbackMessage):
+        """Receive feedback from local_planner's action server as it completes a goal."""
+        # Extract data from feedback message
+        feedback = feedback_msg.feedback
+        # geometry_msgs/PoseStamped current_pose
+        current_pose = feedback.current_pose
+        x, y = current_pose.pose.position.x, current_pose.pose.position.y
+        # TODO builtin_interfaces/Duration navigation_time
+        navigation_time = feedback.navigation_time
+        # TODO builtin_interfaces/Duration estimated_time_remaining
+        estimated_time_remaining = feedback.estimated_time_remaining
+        # TODO int16 number_of_recoveries
+        number_of_recoveries = feedback.number_of_recoveries
+        # float32 distance_remaining
+        distance_remaining = feedback.distance_remaining
+        # int16 number_of_poses_remaining
+        number_of_poses_remaining = feedback.number_of_poses_remaining
+        # Log the feedback data
+        self.get_logger().info(f"at {(round(x, 3), round(y, 3))}\t {round(distance_remaining, 3)}m remaining\t {number_of_poses_remaining} poses left")
+
+
+    def local_plan_result_callback(self, future: Future):
+        """Receive response from local_planner's action server after it completed or failed a goal."""
+        # get result from asynchronous request (future)
+        result: NavigateThroughPoses.Result = future.result().result
+        # get error code from result
+        error_code = result.error_code
+        self.get_logger().info(f"Result from local_planner: {error_code}")
+        # interpret / respond to error code
+        if error_code == 0:
+            self.get_logger().info("Goal completed successfully!")
+            # PHASE ONE -> STOP
+            # PHASE TWO -> SEND NEXT GOAL PATH
+        else:
+            self.get_logger().error("Goal failed :(")
+            # STOP
 
 
     # HELPERS
@@ -110,161 +144,79 @@ class PhaseOneDemo(Node):
         base_x, base_y = lat_lon_to_utm(*BASE_GPS)
         return (x - base_x, y - base_y)
 
-    def calculate_goal_poses(self):
-        """Convert waypoint lat & lon to UTM coordinates relative to origin (base pin)"""
-        for waypoint in WAYPOINTS:
-                # get the absolute UTM of the waypoint
-                abs_goal_utm = lat_lon_to_utm(*waypoint)
-                # get UTM relative to known origin UTM (base pin)
-                rel_goal_utm = self.absolute_to_relative_utm(*abs_goal_utm)
-                # add relative goal UTM to list of poses
-                self.goal_poses.append(rel_goal_utm)
-                self.get_logger().info(f"""Goal: {waypoint}\t Relative UTM: {rel_goal_utm}""")
-        self.get_logger().info("Calculated goal poses.")
-
-    def update_goal_pose(self):
-        """Update goal pose when current goal is reached."""
-        # if within radius of goal -> update
-        if(self.distance_from_goal < WAYPOINT_RADIUS):
-            # If this is not the last pose to navigate
-            if(self.pose_i + 1 < len(self.goal_poses)):
-                # Set the next pose in self.goal_poses
-                self.pose_i += 1
-                self.get_logger().info(f"Reached waypoint #{self.pose_i + 1}")
-            else:
-                self.get_logger().info("Reached last waypoint!")
-            # Stop for 2 seconds before going to next waypoint
-            time.sleep(2)
-
-    def get_goal_pose(self) -> tuple:
-        """Return the current goal pose."""
-        return self.goal_poses[self.pose_i]
+    def lat_lon_to_relative_utm(self, lat: float, lon: float) -> tuple:
+        """Convert lat & lon to UTM coordinate relative to origin (base pin)"""
+        # convert lat & lon to absolute UTM
+        abs_goal_utm = lat_lon_to_utm(lat, lon)
+        # return UTM relative to known origin UTM (base pin)
+        return self.absolute_to_relative_utm(*abs_goal_utm)
     
-    def calculate_distance_from_goal(self):
-        """Calculate the distance between the current position and the current goal pose."""
-        # calculate distance remaining from goal using odom data
-        current_pose = self.current_odom.pose.pose.position
-        odom_distance_from_goal = math.dist(
-            (current_pose.x, current_pose.y),
-            self.get_goal_pose()
-        )
-        # calculate distance remaining from goal using gps data
-        gps_distance_from_goal = haversine(
-            lat1 = self.current_gps.latitude,
-            lon1 = self.current_gps.longitude,
-            lat2 = WAYPOINTS[self.pose_i][0],
-            lon2 = WAYPOINTS[self.pose_i][1]
-        )
-        # select the calculation with the smallest result (will be most accurate in nearly every case)
-        self.distance_from_goal = odom_distance_from_goal
-        # min(
-        #     odom_distance_from_goal,
-        #     gps_distance_from_goal
-        # )
-
     def reset(self):
         """Reset goal poses and current goal pose."""
         self.goal_poses = []
-        self.pose_i = 0
         self.get_logger().info("Reset.")
 
 
+    # PHASE 1 METHODS
+
+    def send_waypoint_path_goal(self):
+        """Phase 1 method: send path with hardcoded waypoints."""
+        # calculate relative UTM coordinates of waypoints (origin at the base pin)
+        rel_goal_utms = [self.lat_lon_to_relative_utm(*waypoint) for waypoint in WAYPOINTS]
+        # construct pose messages with relative UTM coordinates
+        goal_poses = [
+            PoseStamped(pose = Pose(position = Point(
+                x = rel_goal_utm[0], 
+                y = rel_goal_utm[1],
+                z = 0.0
+            )))
+            for rel_goal_utm in rel_goal_utms
+        ]
+        self.get_logger().info("Calculated goal poses.")
+        # debugging info
+        for i, goal_pose in enumerate(rel_goal_utms):
+            self.get_logger().info(f"{i+1}: {goal_pose}")
+        # construct action goal message
+        goal_msg = NavigateThroughPoses.Goal(
+            poses = goal_poses,
+            behavior_tree = "Not Implemented"
+        )
+        # send goal message to local_planner's action server
+        self.send_local_plan_goal(goal_msg)
+
+        
     # TIMER CALLBACKS
 
-    def publish_local_plan(self):
-        """Publish path based on the current goal pose to local_planner node."""
-        # Wait for odom and initial gps data
-        if not self.current_odom or not self.initial_gps:
-            self.get_logger().info("Waiting for odometry and initial gps.")
-            return
-        # Wait for autonomous mode to be True
-        if not self.is_autonomous_mode:
-            self.get_logger().info("Waiting for autonomous mode.")
-            return
-        self.ready_to_ping = True
-        # If the goal poses need to be calculated
-        if len(self.goal_poses) == 0:
-            self.calculate_goal_poses()
-
-        # Set pure_pursuit_controller mode to False
-        self.is_pure_pursuit_controller_mode_publisher.publish(Bool(data=False))
-
-        # calculate the current distance from goal
-        self.calculate_distance_from_goal()
-
-        # CREATE PATH
-        # Configure current pose-stamp
-        current_pose_stamp = PoseStamped(
-            pose = self.current_odom.pose.pose
-        )
-        # Configure goal pose-stamp
-        self.update_goal_pose()
-        goal_pose_stamp = PoseStamped(
-            pose = Pose(
-                position = Point(
-                    x = self.get_goal_pose()[0], 
-                    y = self.get_goal_pose()[1]
-                )
-            )
-        )
-        # Create path from poses
-        path = Path(
-            poses = [
-            #     PoseStamped(
-            #         pose = Pose(
-            #             position = Point(
-            #                 x = goal_pose[0], 
-            #                 y = goal_pose[1]
-            #             )
-            #         )
-            #     ) for goal_pose in self.goal_poses
-            # ]
-            # [
-                # current_pose_stamp,
-                goal_pose_stamp, # Why do we need 3 of the same goal poses in the path?
-                # goal_pose_stamp,
-                # goal_pose_stamp
-            ]
-        )
-        # PUBLISH PATH TO LOCAL_PLANNER
-        self.local_plan_publisher.publish(path)
-
-    def publish_waypoint_ping(self):
-        """Publish waypoint info to splunk_logger node and gps_plotter node."""
-        # wait until publish_local_plan() has created the data to publish
-        if self.ready_to_ping:
-            roll, pitch, yaw = quaternion_to_euler(self.current_odom.pose.pose.orientation)
-            yaw_degrees = math.degrees(yaw)
-            ping = WaypointMsg(
-                waypoint_number = self.pose_i+1,
-                distance = self.distance_from_goal,
-                yaw = yaw_degrees # orientation around the vertical axis
-            )
-            # log so that you can see realtime messages on monitor in phaseOne terminal
-            lat = self.initial_gps.latitude
-            long = self.initial_gps.longitude
-            lat_str = f"{int(lat)}° {((lat - int(lat)) * 60)}"
-            long_str = f"{int(long)}° {((long - int(long)) * 60)}"
-            # self.get_logger().info(f"GPS: {(lat_str, long_str)}\tHeading: {yaw_degrees}°\tDistance: {self.distance_from_goal}\tWaypoint #{self.pose_i+1}")
-            self.ping_publisher.publish(ping)
+    # def publish_waypoint_ping(self):
+    #     """Publish waypoint info to splunk_logger node and gps_plotter node."""
+    #     # wait until publish_local_plan() has created the data to publish
+    #     if self.ready_to_ping:
+    #         roll, pitch, yaw = quaternion_to_euler(self.current_odom.pose.pose.orientation)
+    #         yaw_degrees = math.degrees(yaw)
+    #         ping = WaypointMsg(
+    #             waypoint_number = self.pose_i+1,
+    #             distance = self.distance_from_goal,
+    #             yaw = yaw_degrees # orientation around the vertical axis
+    #         )
+    #         # log so that you can see realtime messages on monitor in phaseOne terminal
+    #         lat = self.initial_gps.latitude
+    #         long = self.initial_gps.longitude
+    #         lat_str = f"{int(lat)}° {((lat - int(lat)) * 60)}"
+    #         long_str = f"{int(long)}° {((long - int(long)) * 60)}"
+    #         # self.get_logger().info(f"GPS: {(lat_str, long_str)}\tHeading: {yaw_degrees}°\tDistance: {self.distance_from_goal}\tWaypoint #{self.pose_i+1}")
+    #         self.ping_publisher.publish(ping)
     
 
     # SUBSCRIBER CALLBACKS
-    
-    def odom_callback(self, odom: Odometry):
-        """Update global odometry."""
-        self.current_odom = odom
-
-    def gps_callback(self, msg: NavSatFix):
-        """Update initial and current GPS."""
-        # Wait for autonous mode -> then get initial gps
-        if self.is_autonomous_mode:
-            if not self.initial_gps:
-                self.initial_gps = msg
-            self.current_gps = msg
 
     def is_autonomous_mode_callback(self, msg: Bool):
         """Update if in autonomous mode."""
+        # if manual to autonomous -> send goal
+        if not self.is_autonomous_mode and msg.data:
+
+            # PHASE 1
+            self.send_waypoint_path_goal()
+
         # if autonomous to manual -> reset
         if self.is_autonomous_mode and not msg.data:
             self.reset()
